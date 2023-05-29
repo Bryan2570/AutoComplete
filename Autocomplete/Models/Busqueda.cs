352f14c6dbbc658f9be659e5303cbf8f8e1642d9
BrayanRojasLemus <brayanrojas@wposs.com>
2023-05-29 10:55:50 -0500

    Autocomplete JQuery ASP.NET

---
+++ Autocomplete/Models/Busqueda.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Autocomplete.Models
+{
+    public class Busqueda
+    {
+        public int value { get; set; }
+        public string label { get; set; }
+        public decimal precio { get; set; }
+        public string descripcion { get; set; }
+
+    }
+}
